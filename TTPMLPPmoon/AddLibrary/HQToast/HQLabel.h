f3b84801e2cbc95b79568fe60612bc204ef91b83
Ropponngi <THU-Tokyo@outlook.com>
2021-02-10 21:41:56 +0800

    Create HQLabel.h

---
+++ TTPMLPPmoon/AddLibrary/HQToast/HQLabel.h
@@ -0,0 +1,13 @@
+//
+//  HQLabel.h
+//  HQMytoast
+//
+//  Created by HQ on 2018/4/23.
+//  Copyright © 2018年 HQ. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HQLabel : UILabel
+- (void)setToastText:(NSString *)text LabelFrame:(CGPoint)labelFrame;
+@end
