9d85486f7aba15f9b9e413e27fcc6da059c6ab14
Ropponngi <THU-Tokyo@outlook.com>
2021-02-10 22:07:09 +0800

    Create TTPMLP_SyntheticViewController.h

---
+++ TTPMLPPmoon/TTPMLP_Controller/合成照片/TTPMLP_SyntheticViewController.h
@@ -0,0 +1,20 @@
+//
+//  TTPMLP_UploadViewController.h
+//  FMLPFigureMonster
+//
+//  Created by apple on 2019/6/12.
+//  Copyright © 2019 feceYouCompany. All rights reserved.
+//图片上传选择器合图
+
+#import "TTPMLP_NavBaseViewController.h"
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface TTPMLP_SyntheticViewController : TTPMLP_NavBaseViewController
+
+/// 图片数组
+@property (nonatomic, strong) NSMutableArray *photoArr;
+
+@end
+
+NS_ASSUME_NONNULL_END
