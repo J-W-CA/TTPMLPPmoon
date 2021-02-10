879684262018fd38e8031598b375a84210cbde8d
Ropponngi <THU-Tokyo@outlook.com>
2021-02-10 22:07:47 +0800

    Create TTPMLPPmoonTests.m

---
+++ TTPMLPPmoonTests/TTPMLPPmoonTests.m
@@ -0,0 +1,37 @@
+//
+//  FMLPFigureMonsterTests.m
+//  FMLPFigureMonsterTests
+//
+//  Created by apple on 2019/6/12.
+//  Copyright © 2019 feceYouCompany. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface FMLPFigureMonsterTests : XCTestCase
+
+@end
+
+@implementation FMLPFigureMonsterTests
+
+- (void)setUp {
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
