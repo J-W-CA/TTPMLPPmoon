8ffbd8f696c270337dde78f8a5425b7017d76000
Ropponngi <THU-Tokyo@outlook.com>
2021-02-10 22:07:17 +0800

    Create TTPMLP_ScrawlPanelView.h

---
+++ TTPMLPPmoon/TTPMLP_Controller/合成照片/View/马赛克/TTPMLP_ScrawlPanelView.h
@@ -0,0 +1,19 @@
+//
+//  TTPMLP_ImageScrollView.h
+//  FMLPFigureMonster
+//
+//  Created by mac on 2019/6/29.
+//  Copyright © 2019 feceYouCompany. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface TTPMLP_ScrawlPanelView : UIView
+
+@property (nonatomic, assign) BOOL flag;
+
+@end
+
+NS_ASSUME_NONNULL_END
