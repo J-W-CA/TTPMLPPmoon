3c4d6ccc1b05e63763a1b158f0fea176092e5b67
Ropponngi <THU-Tokyo@outlook.com>
2021-02-10 22:07:53 +0800

    Create TTPMLP_UploadViewController.h

---
+++ TTPMLPPmoon/TTPMLP_Controller/HomeVC/TTPMLP_UploadViewController.h
@@ -0,0 +1,17 @@
+//
+//  TTPMLP_UploadViewController.h
+//  FMLPFigureMonster
+//
+//  Created by apple on 2019/6/12.
+//  Copyright © 2019 feceYouCompany. All rights reserved.
+//图片上传选择器合图
+
+#import "TTPMLP_NavBaseViewController.h"
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface TTPMLP_UploadViewController : TTPMLP_NavBaseViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
