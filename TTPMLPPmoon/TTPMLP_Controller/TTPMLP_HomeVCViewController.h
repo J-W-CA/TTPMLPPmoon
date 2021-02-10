2643fa5c5c4c8119cb70827d5282fe6144b13031
Ropponngi <THU-Tokyo@outlook.com>
2021-02-10 22:07:52 +0800

    Create TTPMLP_HomeVCViewController.h

---
+++ TTPMLPPmoon/TTPMLP_Controller/TTPMLP_HomeVCViewController.h
@@ -0,0 +1,21 @@
+//
+//  TTPMLP_HomeVCViewController.h
+//  FMLPFigureMonster
+//
+//  Created by apple on 2019/6/12.
+//  Copyright © 2019 feceYouCompany. All rights reserved.
+//
+
+#import "TTPMLP_NavBaseViewController.h"
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface TTPMLP_HomeVCViewController : TTPMLP_NavBaseViewController
+
+@property (weak, nonatomic) IBOutlet UILabel *labSktich;
+@property (weak, nonatomic) IBOutlet UILabel *labPotho;
+@property (weak, nonatomic) IBOutlet UILabel *labFilter;
+
+@end
+
+NS_ASSUME_NONNULL_END
