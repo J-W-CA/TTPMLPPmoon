89af4fdcd74a615f793088ade1bf07debf2eecce
Ropponngi <THU-Tokyo@outlook.com>
2021-02-10 22:07:57 +0800

    Create TTPMLP_PhotoViewController.h

---
+++ TTPMLPPmoon/TTPMLP_Controller/HomeVC/TTPMLP_PhotoViewController.h
@@ -0,0 +1,17 @@
+//
+//  TTPMLP_PhotoViewController.h
+//  FMLPFigureMonster
+//
+//  Created by apple on 2019/6/14.
+//  Copyright © 2019 feceYouCompany. All rights reserved.
+//
+
+#import "TTPMLP_NavBaseViewController.h"
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface TTPMLP_PhotoViewController : TTPMLP_NavBaseViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
