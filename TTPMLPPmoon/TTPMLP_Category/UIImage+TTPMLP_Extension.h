d9c81ff88d0da9ee88ee8e95db1a695c456e28c7
Ropponngi <THU-Tokyo@outlook.com>
2021-02-10 22:06:10 +0800

    Create UIImage+TTPMLP_Extension.h

---
+++ TTPMLPPmoon/TTPMLP_Category/UIImage+TTPMLP_Extension.h
@@ -0,0 +1,19 @@
+//
+//  UIImage+TTPMLP_Extension.h
+//  FMLPFigureMonster
+//
+//  Created by mac on 2019/7/9.
+//  Copyright © 2019 feceYouCompany. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface UIImage (TTPMLP_Extension)
+
++ (UIImage*)createImageWithColor:(UIColor*) color;
+
+@end
+
+NS_ASSUME_NONNULL_END
