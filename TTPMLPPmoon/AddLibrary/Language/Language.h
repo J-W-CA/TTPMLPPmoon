1abaf8469cefc4477357c29d0c7f12645dc99e0f
Ropponngi <THU-Tokyo@outlook.com>
2021-02-10 21:42:25 +0800

    Create Language.h

---
+++ TTPMLPPmoon/AddLibrary/Language/Language.h
@@ -0,0 +1,20 @@
+//
+//  Language.h
+//  Language
+//
+//  Created by whde on 16/1/13.
+//  Copyright © 2016年 Whde. All rights reserved.
+//
+// GitHub:https://github.com/whde/WhdeLocalized
+
+#import <UIKit/UIKit.h>
+#import "CLanguageUtil.h"
+//! Project version number for Language.
+FOUNDATION_EXPORT double LanguageVersionNumber;
+
+//! Project version string for Language.
+FOUNDATION_EXPORT const unsigned char LanguageVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Language/PublicHeader.h>
+
+
