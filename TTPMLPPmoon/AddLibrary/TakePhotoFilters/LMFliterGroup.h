80f5f7834e7190a658e7b4d1cd3e7c5545d7830d
Ropponngi <THU-Tokyo@outlook.com>
2021-02-10 21:58:05 +0800

    Create LMFliterGroup.h

---
+++ TTPMLPPmoon/AddLibrary/TakePhotoFilters/LMFliterGroup.h
@@ -0,0 +1,18 @@
+//
+//  LMFliterGroup.h
+//  Test1030
+//
+//  Created by xx11dragon on 15/11/4.
+//  Copyright © 2015年 xx11dragon. All rights reserved.
+//
+
+#import "GPUImageFilterGroup.h"
+
+@interface GPUImageFilterGroup(addTitleColor)
+
+@property (nonatomic , copy) NSString *title;
+
+@property (nonatomic , strong) UIColor *color;
+
+
+@end
