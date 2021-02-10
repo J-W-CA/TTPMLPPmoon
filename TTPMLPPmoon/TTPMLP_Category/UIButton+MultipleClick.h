1f5dd1f834bac34871402e98466ace89bd7c46a4
Ropponngi <THU-Tokyo@outlook.com>
2021-02-10 22:06:05 +0800

    Create UIButton+MultipleClick.h

---
+++ TTPMLPPmoon/TTPMLP_Category/UIButton+MultipleClick.h
@@ -0,0 +1,18 @@
+//
+//  UIButton+MultipleClick.h
+//  ButtonMultipleClick
+//
+//  Created by 陈阳阳 on 16/9/1.
+//  Copyright © 2016年 cyy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UIButton (MultipleClick)
+
+/**
+ *  时间间隔
+ */
+@property (nonatomic, assign) NSTimeInterval timeInterval;
+
+@end
