6e3506ca5c44aec874181b322475204430661ed5
Ropponngi <THU-Tokyo@outlook.com>
2021-02-10 22:05:52 +0800

    Create AppDelegate.h

---
+++ TTPMLPPmoon/TTPMLP_Application/AppDelegate.h
@@ -0,0 +1,22 @@
+//
+//  AppDelegate.h
+//  FMLPFigureMonster
+//
+//  Created by apple on 2019/6/12.
+//  Copyright © 2019 feceYouCompany. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+//#import <CoreData/CoreData.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+//@property (readonly, strong) NSPersistentContainer *persistentContainer;
+
+//- (void)saveContext;
+
+
+@end
+
